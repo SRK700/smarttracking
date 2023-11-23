26ebad9eda4f5103e0524de5e9f502341e5c31cd
SRK700 <papana.210@gmail.com>
2023-11-23 11:36:01 +0700

    -m -a

--- lib/menu_health.dart
+++ lib/menu_health.dart
@@ -11,10 +11,38 @@ class MenuHealthScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image.asset(
-              'images/health_icon.png', // Adjust the path accordingly
-              width: 200.0, // Adjust the width as needed
-              height: 200.0, // Adjust the height as needed
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(
+                  'images/heart.png',
+                  width: 100.0,
+                  height: 100.0,
+                ),
+                SizedBox(width: 16.0),
+                Image.asset(
+                  'images/fever.png',
+                  width: 100.0,
+                  height: 100.0,
+                ),
+              ],
+            ),
+            SizedBox(height: 16.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(
+                  'images/blood-pressure.png',
+                  width: 100.0,
+                  height: 100.0,
+                ),
+                SizedBox(width: 16.0),
+                Image.asset(
+                  'images/sleep.png',
+                  width: 100.0,
+                  height: 100.0,
+                ),
+              ],
             ),
             SizedBox(height: 16.0),
             Text(
